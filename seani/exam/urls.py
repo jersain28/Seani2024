101c0071013acc78c7e18ee20638d0084761260b
unknown <jersainmarin20679@gmail.com>
2024-03-13 14:45:34 -0600

    08 Implements navigation in questions exam

--- seani/exam/urls.py
+++ seani/exam/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'exam'
 urlpatterns = [
-    path('create/', views.create, name = 'create'),
     path('', views.home, name = 'home'),
+    path('module/<int:m_id>/question/', views.question, name = 'question'),
+    path('module/<int:m_id>/question/<int:q_id>/', views.question, name = 'question'),
+    path('create', views.create, name = 'create'),
 ]
--- seani/exam/views.py
+++ seani/exam/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Stage, Exam
 from django.contrib.auth.models import User
 from career.models import Career
@@ -9,6 +9,32 @@ from .forms import CandidateForm
 def home(request):
     user = request.user
     return render(request, 'exam/home.html', {"user": user})
+
+def question(request, m_id, q_id=1):
+    user = request.user
+    exam = user.exam
+
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        questions = exam.breakdown_set.filter(question__module_id = m_id)
+        question = questions[q_id-1]
+        question.answer = answer
+        question.save()
+        return redirect('exam:question', m_id, q_id+1)
+    
+    try:
+        questions = exam.breakdown_set.filter(question__module_id = m_id)
+        question = questions[q_id-1].question
+        answer = questions[q_id-1].answer
+        return render(request, 'exam/question.html',
+                    {
+                        "question": question,
+                        "m_id": m_id,
+                        "q_id": q_id,
+                        "answer": answer,
+                    })
+    except IndexError:
+        return redirect('exam:home')
 
 def create(request):
     if request.method == 'GET':
